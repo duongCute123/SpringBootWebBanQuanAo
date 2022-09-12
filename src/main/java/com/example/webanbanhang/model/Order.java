a5fec148234f1587875161040951dd7e3568030b
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2022-09-12 21:19:09 +0700

    finish model

--- src/main/java/com/example/webanbanhang/model/Order.java
+++ src/main/java/com/example/webanbanhang/model/Order.java
@@ -1,5 +1,120 @@
 package com.example.webanbanhang.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "Orders", uniqueConstraints = { @UniqueConstraint(columnNames = "Order_Num") })
 public class Order {
+	@Id
+	@Column(name = "ID")
+	String id;
+	@Column(name = "Order_Date")
+	Date oderDate;
+	@Column(name = "Order_Num")
+	int orderNum;
+	@Column(name = "Amount")
+	double amount;
+	@Column(name = "Customer_Name")
+	String customerName;
+	@Column(name = "Customer_Address")
+	String customerAddress;
+	@Column(name = "Customer_Email")
+	String customerEmail;
+	@Column(name = "Customer_Phone")
+	String customerPhone;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getOderDate() {
+		return oderDate;
+	}
+
+	public void setOderDate(Date oderDate) {
+		this.oderDate = oderDate;
+	}
+
+	public int getOrderNum() {
+		return orderNum;
+	}
+
+	public void setOrderNum(int orderNum) {
+		this.orderNum = orderNum;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+	public String getCustomerPhone() {
+		return customerPhone;
+	}
+
+	public void setCustomerPhone(String customerPhone) {
+		this.customerPhone = customerPhone;
+	}
+
+	public Order(String id, Date oderDate, int orderNum, double amount, String customerName, String customerAddress,
+			String customerEmail, String customerPhone) {
+		super();
+		this.id = id;
+		this.oderDate = oderDate;
+		this.orderNum = orderNum;
+		this.amount = amount;
+		this.customerName = customerName;
+		this.customerAddress = customerAddress;
+		this.customerEmail = customerEmail;
+		this.customerPhone = customerPhone;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", oderDate=" + oderDate + ", orderNum=" + orderNum + ", amount=" + amount
+				+ ", customerName=" + customerName + ", customerAddress=" + customerAddress + ", customerEmail="
+				+ customerEmail + ", customerPhone=" + customerPhone + "]";
+	}
+
+	public Order() {
+		super();
+	}
 
 }
--- src/main/java/com/example/webanbanhang/model/Product.java
+++ src/main/java/com/example/webanbanhang/model/Product.java
@@ -1,22 +1,90 @@
 package com.example.webanbanhang.model;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "Products")
 public class Product {
 	@Id
-	@Column(name ="Code",length = 20,nullable = false)
+	@Column(name = "Code", length = 20, nullable = false)
 	String code;
-	@Column(name = "Name",length = 255,nullable = false)
+	@Column(name = "Name", length = 255, nullable = false)
 	String name;
+	@Column(name = "Price", nullable = false)
 	double price;
-	byte [] image;
+	@Lob
+	@Column(name = "Image", length = Integer.MAX_VALUE, nullable = false)
+	byte[] image;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "Create_Date", nullable = false)
 	Date createDate;
-	
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Product(String code, String name, double price, byte[] image, Date createDate) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		this.image = image;
+		this.createDate = createDate;
+	}
+
+	public Product() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Product [code=" + code + ", name=" + name + ", price=" + price + ", image=" + Arrays.toString(image)
+				+ ", createDate=" + createDate + "]";
+	}
+
 }
